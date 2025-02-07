697c3712a9f7ba33c7150224af67d6eb24e77a39
Jamie Pearcey <jpearcey@live.co.uk>
2025-02-07 12:36:02 +0000

    Update css type definition

--- packages/react-scoped-stylesheet/src/global.d.ts
+++ packages/react-scoped-stylesheet/src/global.d.ts
@@ -1,14 +1,6 @@
 declare module '*.scoped.css' {
-    const scopeClass: string;
-  export { scopeClass };
-}
-
-declare module '*.scoped.scss' {
   const scopeClass: string;
   export { scopeClass };
-}
-
-declare module '*.scoped.sass' {
-  const scopeClass: string;
-  export { scopeClass };
+  const defaultExport: any;
+  export default defaultExport;
 }
--- packages/react-scoped-stylesheet/src/index.ts
+++ packages/react-scoped-stylesheet/src/index.ts
@@ -1,2 +1,3 @@
+/// <reference path="./global.d.ts" />
 export { viteScopedStylesPlugin } from './vite-plugin';
 export { nextScopedStylesPlugin } from './next-plugin';
